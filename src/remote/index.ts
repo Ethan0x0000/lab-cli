399a3d0f99987ebee7a8772f058de8fd0fa1b1db
Ethan0x0000 <3352979663@qq.com>
2026-04-29 13:24:36 +0800

    feat: add remote module barrel export

---
+++ src/remote/index.ts
@@ -0,0 +1,4 @@
+export type { CommandResult, RemoteExecution } from './types.js'
+export { SSHExecution } from './ssh-execution.js'
+export { LocalExecution } from './local-execution.js'
+export { MockExecution } from './mock-execution.js'
